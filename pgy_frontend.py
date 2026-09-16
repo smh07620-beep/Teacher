a4441df569888dc51bc979ea4de62edd8692085a
smh07620-beep <smh07620@gmail.com>
2026-09-16 15:17:41 +0800

    refactor: load quiz material linker module

--- pgy_frontend.py
+++ pgy_frontend.py
@@ -95,6 +95,8 @@ def register_pgy_frontend(app):
                 body_assets.append('<script defer src="/admin-materials.js?v=7105"></script>')
             if "/admin-question-bank.js" not in html:
                 body_assets.append('<script defer src="/admin-question-bank.js?v=7106"></script>')
+            if "/admin-quiz-materials.js" not in html:
+                body_assets.append('<script defer src="/admin-quiz-materials.js?v=7118"></script>')
             if "/admin-jobs.js" not in html:
                 body_assets.append('<script defer src="/admin-jobs.js?v=7107"></script>')
             if "/admin-material-upload.js" not in html:
