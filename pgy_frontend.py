edb0afc6056d676ca8d96d97b3a96721e3669d67
smh07620-beep <smh07620@gmail.com>
2026-09-16 11:57:47 +0800

    refactor: load admin system storage module

--- pgy_frontend.py
+++ pgy_frontend.py
@@ -52,6 +52,8 @@ def register_pgy_frontend(app):
                 body_assets.append('<script defer src="/admin-people.js?v=7102"></script>')
             if "/admin-announcements.js" not in html:
                 body_assets.append('<script defer src="/admin-announcements.js?v=7103"></script>')
+            if "/admin-system.js" not in html:
+                body_assets.append('<script defer src="/admin-system.js?v=7104"></script>')
             if head_assets and "</head>" in html:
                 html = html.replace("</head>", "\n".join(head_assets) + "\n</head>", 1)
             if body_assets and "</body>" in html:
